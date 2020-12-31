9c7d9cb486a09e4280edeeb1e2d7c2e7a6e3c297
Bencoderus <bencoderus@gmail.com>
2020-12-31 22:05:38 +0100

    Add soft delete support

--- src/Models/Client.php
+++ src/Models/Client.php
@@ -3,8 +3,11 @@
 namespace Bencoderus\MinAuth\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
+    use SoftDeletes;
+
     protected $guarded = [];
 }
