b65cc31bbba7a83b2e5b1d2c2d6ae7ee4dcf6189
Bencoderus <bencoderus@gmail.com>
2020-12-31 19:59:34 +0100

    Add support for PHP 8

--- src/MinAuth.php
+++ src/MinAuth.php
@@ -11,14 +11,17 @@ class MinAuth
      * Create a client using the client name.
      *
      * @param string $name
+     * @param string $ip
+     * @param bool $isBlacklisted
      * @return \Bencoderus\MinAuth\Models\Client
      */
-    public static function createClient(string $name): Client
+    public static function createClient(string $name, string $ip = "127.0.0.1", bool $isBlacklisted = false): Client
     {
         return Client::create([
             'name' => $name,
             'api_key' => self::generateApiKey(),
-            'ip' => '127.0.0.1',
+            'ip' => $ip,
+            'is_blacklisted' => $isBlacklisted,
         ]);
     }
 
